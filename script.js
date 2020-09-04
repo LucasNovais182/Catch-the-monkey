b2c0ca8c83ae20c45196969c1de5fca48a89733c
Lucas Oliveira <lucas.novais21@outlook.com>
2020-09-04 15:56:50 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+var stick = [document.getElementsByClassName('stick')[0], document.getElementsByClassName('stick')[1]]
+var score = document.getElementsByTagName('p')[0]
+stick[0].addEventListener('click', () => {
+    score.innerHTML = 1 + Number(score.innerHTML)
+})
+stick[1].addEventListener('click', () => {
+    score.innerHTML = 1 + Number(score.innerHTML)
+})
+
+setInterval(() => {
+    stick[0].style.top = `${Math.random() * (100 - 10) - 10}%`
+    stick[1].style.top = `${Math.random() * (100 - 10) - 10}%`
+}, 1400);
