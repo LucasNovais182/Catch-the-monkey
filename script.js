461b875a54c908be482f2a08bd9ac80978f47136
Lucas <lucas.novais21@outlook.com>
2020-09-18 22:03:21 -0300

    feat: monkeys are introduced at the top and bottom

--- script.js
+++ script.js
@@ -1,23 +1,46 @@
-var stick = [document.getElementsByClassName('stick')[0], document.getElementsByClassName('stick')[1]]
+var stick = [document.getElementsByClassName('stick')[0], document.getElementsByClassName('stick')[1], document.getElementsByClassName('stick')[2], document.getElementsByClassName('stick')[3]]
 var score = document.getElementsByTagName('p')[0]
+
 stick[0].addEventListener('click', () => {
     score.innerHTML = 1 + Number(score.innerHTML)
 })
 stick[1].addEventListener('click', () => {
     score.innerHTML = 1 + Number(score.innerHTML)
 })
+stick[2].addEventListener('click', () => {
+    score.innerHTML = 1 + Number(score.innerHTML)
+})
+stick[3].addEventListener('click', () => {
+    score.innerHTML = 1 + Number(score.innerHTML)
+})
 
 setInterval(() => {
     stick[0].style.display = 'block';
     stick[1].style.display = 'block';
+    stick[2].style.display = 'block';
+    stick[3].style.display = 'block';
 
-    if(Math.random() * (10 - 0) > 5){
+    if (Math.random() * (10 - 0) < 2) {
         stick[0].style.top = `${Math.random() * (100 - 10) - 10}%`
         stick[1].style.display = 'none';
-    }else{
+        stick[2].style.display = 'none';
+        stick[3].style.display = 'none';
+    } else if (Math.random() * (10 - 0) > 2 && Math.random() * (10 - 0) < 4) {
         stick[1].style.top = `${Math.random() * (100 - 10) - 10}%`
         stick[0].style.display = 'none';
-    }   
+        stick[2].style.display = 'none';
+        stick[3].style.display = 'none';
+    } else if (Math.random() * (10 - 0) > 4 && Math.random() * (10 - 0) < 6) {
+        stick[2].style.right = `${Math.random() * (100 - 10) - 10}%`
+        stick[0].style.display = 'none';
+        stick[1].style.display = 'none';
+        stick[3].style.display = 'none';
+    } else {
+        stick[3].style.right = `${Math.random() * (100 - 10) - 10}%`
+        stick[0].style.display = 'none';
+        stick[1].style.display = 'none';
+        stick[2].style.display = 'none';
+    }
     
      
-}, 1400);
+}, 2000);
